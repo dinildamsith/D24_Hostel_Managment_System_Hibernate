ff2dd473abdc35ad30b815cb2e4d8e7ed4cd4d1a
Dinil Damsith <dinildamsith52@gmail.com>
2023-08-16 14:01:43 +0530

    Set actions room controller

--- src/main/java/lk/ijse/d24_hostel_managment_system/Controller/RoomFormController.java
+++ src/main/java/lk/ijse/d24_hostel_managment_system/Controller/RoomFormController.java
@@ -1,4 +1,54 @@
 package lk.ijse.d24_hostel_managment_system.Controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class RoomFormController {
+    @FXML
+    private JFXTextField roomTypeIdTxt;
+
+    @FXML
+    private JFXTextField keyMoneyTxt;
+
+    @FXML
+    private JFXTextField roomTypeTxt;
+
+    @FXML
+    private JFXTextField roomQtyTxt;
+
+    @FXML
+    private TableView<?> roomTable;
+
+    @FXML
+    private TableColumn<?, ?> roomTypeIdColumn;
+
+    @FXML
+    private TableColumn<?, ?> roomTypeColumn;
+
+    @FXML
+    private TableColumn<?, ?> keyMoneyColumn;
+
+    @FXML
+    private TableColumn<?, ?> roomQtyColumn;
+
+    @FXML
+    private JFXButton saveBtn;
+
+    @FXML
+    private JFXButton deleteBtn;
+
+    @FXML
+    void deleteBtnOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void saveBtnOnAction(ActionEvent event) {
+
+    }
+
 }
--- src/main/java/lk/ijse/d24_hostel_managment_system/Controller/StudentFormController.java
+++ src/main/java/lk/ijse/d24_hostel_managment_system/Controller/StudentFormController.java
@@ -1,4 +1,81 @@
 package lk.ijse.d24_hostel_managment_system.Controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
 public class StudentFormController {
+
+    @FXML
+    private JFXTextField studentNameTxt;
+
+    @FXML
+    private JFXTextField StudentAddressTxt;
+
+    @FXML
+    private JFXTextField contactNumberTxt;
+
+    @FXML
+    private CheckBox maleCheckBox;
+
+    @FXML
+    private CheckBox femaleCheckBox;
+
+    @FXML
+    private DatePicker birthdatPiker;
+
+    @FXML
+    private Label studentIdLbl;
+
+    @FXML
+    private TableView<?> studentTable;
+
+    @FXML
+    private TableColumn<?, ?> idColumn;
+
+    @FXML
+    private TableColumn<?, ?> nameColum;
+
+    @FXML
+    private TableColumn<?, ?> addressColumn;
+
+    @FXML
+    private TableColumn<?, ?> contactColumn;
+
+    @FXML
+    private TableColumn<?, ?> birthColumn;
+
+    @FXML
+    private TableColumn<?, ?> genderColumn;
+
+    @FXML
+    private JFXButton saveBtn;
+
+    @FXML
+    private JFXButton deleteBtn;
+
+
+    @FXML
+    void saveBtnOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void deleteBtnOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void femaleCheckBoxOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void maleCheckBoxOnAction(ActionEvent event) {
+
+    }
+
+
 }
