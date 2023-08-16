dfb5a3fa3bae9090bd5950353e31c0aa6b2b64ac
Dinil Damsith <dinildamsith52@gmail.com>
2023-08-16 22:27:57 +0530

    set reservation controller actions

--- src/main/java/lk/ijse/d24_hostel_managment_system/Controller/ReservationFormController.java
+++ src/main/java/lk/ijse/d24_hostel_managment_system/Controller/ReservationFormController.java
@@ -1,4 +1,67 @@
 package lk.ijse.d24_hostel_managment_system.Controller;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
 public class ReservationFormController {
+    @FXML
+    private CheckBox paidCheckBox;
+
+    @FXML
+    private CheckBox unPaidCheckBox;
+
+    @FXML
+    private DatePicker datePicker;
+
+    @FXML
+    private ComboBox<?> studentIdComboBox;
+
+    @FXML
+    private ComboBox<?> roomTypeIdComboBox;
+
+    @FXML
+    private Label reservationIdLbl;
+
+    @FXML
+    private TableView<?> reservationTable;
+
+    @FXML
+    private TableColumn<?, ?> res_IdColumn;
+
+    @FXML
+    private TableColumn<?, ?> stu_idColumn;
+
+    @FXML
+    private TableColumn<?, ?> roomTypeIdColumn;
+
+    @FXML
+    private TableColumn<?, ?> roomTypeColumn;
+
+    @FXML
+    private TableColumn<?, ?> dateColumn;
+
+    @FXML
+    private TableColumn<?, ?> statusColumn;
+
+    @FXML
+    private JFXButton saveBtn;
+
+    @FXML
+    private JFXButton deleteBtn;
+
+    @FXML
+    private Label roomTypeLbl;
+
+    @FXML
+    void delteBtnOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void saveOnAction(ActionEvent event) {
+
+    }
+
 }
