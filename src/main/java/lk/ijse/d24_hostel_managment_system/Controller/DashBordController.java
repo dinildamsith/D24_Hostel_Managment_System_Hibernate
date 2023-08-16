c2c5eba12a1af03b998cacab0f1c432e826b2b01
Dinil Damsith <dinildamsith52@gmail.com>
2023-08-16 13:05:42 +0530

    Add loging button action and edit fxml resource

--- src/main/java/lk/ijse/d24_hostel_managment_system/Controller/DashBordController.java
+++ src/main/java/lk/ijse/d24_hostel_managment_system/Controller/DashBordController.java
@@ -1,4 +1,60 @@
 package lk.ijse.d24_hostel_managment_system.Controller;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class DashBordController {
+    @FXML
+    private AnchorPane anchorpane2;
+
+    @FXML
+    private JFXButton studentBtn;
+
+    @FXML
+    private JFXButton roomsBtn;
+
+    @FXML
+    private JFXButton reservationBtn;
+
+    @FXML
+    private JFXButton logOutBtn;
+
+    @FXML
+    private AnchorPane root;
+
+
+    @FXML
+    void studentBtnOnAction(ActionEvent event) throws IOException {
+
+
+    }
+
+
+
+    @FXML
+    void roomsBtnOnAction(ActionEvent event) {
+
+    }
+
+
+
+    @FXML
+    void reservationBtnOnAction(ActionEvent event) {
+
+
+    }
+
+    @FXML
+    void logoutBtnOnAction(ActionEvent event) {
+
+    }
+
+
+
 }
--- src/main/java/lk/ijse/d24_hostel_managment_system/Controller/loginFormController.java
+++ src/main/java/lk/ijse/d24_hostel_managment_system/Controller/loginFormController.java
@@ -1,4 +1,34 @@
 package lk.ijse.d24_hostel_managment_system.Controller;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class loginFormController {
+
+    @FXML
+    private AnchorPane root;
+
+    @FXML
+    private JFXButton loginBtn;
+
+    @FXML
+    void loginBtnOnAction(ActionEvent event) throws IOException {
+        AnchorPane anchorPane = FXMLLoader.load(getClass().getResource("/view/DashBord.fxml"));
+        Stage stage = new Stage();
+        stage.setScene(new Scene(anchorPane));
+        Stage stage1 = (Stage) root.getScene().getWindow();
+        stage1.close();
+        stage.setTitle("Dashboard");
+        stage.centerOnScreen();
+        stage.show();
+
+    }
+
 }
